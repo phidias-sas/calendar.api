31edb0d5de0c3f03e3a3b506158ad6d66040eade
Santiago Cortes <santiago.cortes@phidias.com.co>
2016-12-22 20:44:21 +0100

    removed stupidity

--- resources/calendar/events/eventId/ics.php
+++ resources/calendar/events/eventId/ics.php
@@ -1,15 +1,18 @@
 <?php return [
     "/calendar/events/{eventId}/ics" => [
-        "get" => "Phidias\Calendar\Event\Ics\Controller->main({eventId})",
-        "filter" => function($response, $output) {
-            $body = new Http\Stream("php://temp", "w");
-            $body->write($output);
+        "get" => [
+            "controller" => "Phidias\Calendar\Event\Ics\Controller->main({eventId})",
 
-            return $response
-                ->header("Content-type",        "text/calendar; charset=utf-8")
-                ->header("Content-Disposition", "attachment; filename=event.ics")
-                ->header("Cache-Control",       "must-revalidate, post-check=0, pre-check=0")
-                ->body($body);
-        }
+            "filter" => function($response, $output) {
+                $body = new Http\Stream("php://temp", "w");
+                $body->write($output);
+
+                return $response
+                    ->header("Content-type",        "text/calendar; charset=utf-8")
+                    ->header("Content-Disposition", "attachment; filename=event.ics")
+                    ->header("Cache-Control",       "must-revalidate, post-check=0, pre-check=0")
+                    ->body($body);
+            }            
+        ]
     ]
 ];
