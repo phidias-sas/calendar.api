2f009aae041dd9d8fb2e84045332ebae87529e37
Santiago Cortes <santiago.cortes@phidias.com.co>
2016-12-22 21:33:02 +0100

    add eventId to attached filename

--- resources/calendar/events/eventId/ics.php
+++ resources/calendar/events/eventId/ics.php
@@ -5,13 +5,13 @@ return [
         "get" => [
             "controller" => "Phidias\Calendar\Event\Ics\Controller->main({eventId})",
 
-            "filter" => function($response, $output) {
+            "filter" => function($response, $output, $eventId) {
                 $body = new Stream("php://temp", "w");
                 $body->write($output);
 
                 return $response
                     ->header("Content-type",        "text/calendar; charset=utf-8")
-                    ->header("Content-Disposition", "attachment; filename=event.ics")
+                    ->header("Content-Disposition", "attachment; filename=event_{$eventId}.ics")
                     ->header("Cache-Control",       "must-revalidate, post-check=0, pre-check=0")
                     ->body($body);
             }            
