30c1e50581afd0d0d178765c1196051ae1f4b4d9
Santiago Cortes <santiago.cortes@phidias.com.co>
2016-12-22 20:42:56 +0100

    serve ics as attached file

--- resources/calendar/events/eventId/ics.php
+++ resources/calendar/events/eventId/ics.php
@@ -1,5 +1,15 @@
 <?php return [
     "/calendar/events/{eventId}/ics" => [
-        "get" => "Phidias\Calendar\Event\Ics\Controller->main({eventId})"
+        "get" => "Phidias\Calendar\Event\Ics\Controller->main({eventId})",
+        "filter" => function($response, $output) {
+            $body = new Http\Stream("php://temp", "w");
+            $body->write($output);
+
+            return $response
+                ->header("Content-type",        "text/calendar; charset=utf-8")
+                ->header("Content-Disposition", "attachment; filename=event.ics")
+                ->header("Cache-Control",       "must-revalidate, post-check=0, pre-check=0")
+                ->body($body);
+        }
     ]
 ];
