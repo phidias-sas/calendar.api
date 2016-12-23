0da33b32180c84acde091220c4a20615c93a8a5d
Santiago Cortes <santiago.cortes@phidias.com.co>
2016-12-23 02:30:05 +0100

    add repetition rules to iCal format

--- src/Event/Ics/Controller.php
+++ src/Event/Ics/Controller.php
@@ -1,16 +1,58 @@
 <?php
 namespace Phidias\Calendar\Event\Ics;
 use Phidias\Calendar\Event\Entity as Event;
+use Phidias\Calendar\Event\Repetition\Entity as Repetition;
 
 class Controller
 {
     public function main($eventId)
     {
-        $event = new Event($eventId);
+        $event = Event::single()
+            ->allAttributes()
+            ->attribute("repetition", Repetition::single()->allAttributes())
+            ->fetch($eventId);
 
         $creationDate = date('Ymd\THis\Z', $event->creationDate);
         $startDate    = date('Ymd\THis\Z', $event->startDate);
         $endDate      = date('Ymd\THis\Z', $event->endDate);
+
+        if ($event->repetition) {
+            $rules = [];
+            switch ($event->repetition->frequency) {
+                case Repetition::FREQUENCY_DAILY:
+                    $rules[] = "FREQ=DAILY";
+                    break;
+
+                case Repetition::FREQUENCY_WEEKLY:
+                    $rules[] = "FREQ=WEEKLY";
+                    $rules[] = "BYSETPOS=" . ceil(date("j", $event->startDate)/7);
+                    $rules[] = "BYDAY=" . strtoupper(substr(date("D", $event->startDate),0,2));
+                    break;
+
+                case Repetition::FREQUENCY_MONTHLY_DAY:
+                    $rules[] = "FREQ=MONTHLY";
+                    $rules[] = "BYMONTHDAY=" . date("j", $event->startDate);
+                    break;
+
+                case Repetition::FREQUENCY_MONTHLY_WEEKDAY:
+                    $rules[] = "FREQ=MONTHLY";
+                    break;
+
+                case Repetition::FREQUENCY_YEARLY:
+                    $rules[] = "FREQ=YEARLY";
+                    break;
+            }
+
+            $rules[] = "INTERVAL={$event->repetition->interval}";
+
+            if ($event->repetition->count) {
+                $rules[] = "COUNT={$event->repetition->count}";
+            }
+
+            $rRule = "\nRRULE:" . implode(";", $rules);
+        } else {
+            $rRule = "";
+        }
 
         $icsContents = "BEGIN:VCALENDAR
 VERSION:2.0
@@ -20,7 +62,7 @@ UID:{$event->id}
 DTSTAMP:{$creationDate}
 DTSTART:{$startDate}
 DTEND:{$endDate}
-SUMMARY:{$event->title}
+SUMMARY:{$event->title}{$rRule}
 END:VEVENT
 END:VCALENDAR";
 
