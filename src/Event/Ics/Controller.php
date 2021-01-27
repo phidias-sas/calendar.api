ae031d4009944113cc4af67955e17385b59d4ae1
Andres Manzano <andres.manzano@outlook.com>
2021-01-27 10:47:02 -0500

    ajustes saltoa con php_EOL

--- src/Event/Ics/Controller.php
+++ src/Event/Ics/Controller.php
@@ -28,47 +28,45 @@ class Controller
         $postData = "";
         
         if($postEvent){
-            $clean_description_html = html_entity_decode($postEvent->post->description);
             $clean_description = html_entity_decode($postEvent->post->description);
             $clean_description = self::abc($clean_description);
             
             $postData = "
-DESCRIPTION:{$clean_description}
-X-ALT-DESC;FMTTYPE=text/html:{$clean_description_html}
-CATEGORY:{$postEvent->post->type->plural}";
+DESCRIPTION:{$clean_description}{PHP_EOL}
+CATEGORY:{$postEvent->post->type->plural}{PHP_EOL}";
         }
         
         if ($event->repetition) {
             $rules = [];
             switch ($event->repetition->frequency) {
                 case Repetition::FREQUENCY_DAILY:
-                    $rules[] = "FREQ=DAILY";
+                    $rules[] = "FREQ=DAILY".PHP_EOL;
                     break;
 
                 case Repetition::FREQUENCY_WEEKLY:
-                    $rules[] = "FREQ=WEEKLY";
-                    $rules[] = "BYDAY=" . strtoupper(substr(date("D", $event->startDate),0,2));
+                    $rules[] = "FREQ=WEEKLY".PHP_EOL;
+                    $rules[] = "BYDAY=" . strtoupper(substr(date("D", $event->startDate),0,2)).PHP_EOL;
                     break;
 
                 case Repetition::FREQUENCY_MONTHLY_DAY:
-                    $rules[] = "FREQ=MONTHLY";
-                    $rules[] = "BYMONTHDAY=" . date("j", $event->startDate);
+                    $rules[] = "FREQ=MONTHLY".PHP_EOL;
+                    $rules[] = "BYMONTHDAY=" . date("j", $event->startDate).PHP_EOL;
                     break;
 
                 case Repetition::FREQUENCY_MONTHLY_WEEKDAY:
-                    $rules[] = "FREQ=MONTHLY";
-                    $rules[] = "BYSETPOS=" . ceil(date("j", $event->startDate)/7);
+                    $rules[] = "FREQ=MONTHLY".PHP_EOL;
+                    $rules[] = "BYSETPOS=" . ceil(date("j", $event->startDate)/7).PHP_EOL;
                     break;
 
                 case Repetition::FREQUENCY_YEARLY:
-                    $rules[] = "FREQ=YEARLY";
+                    $rules[] = "FREQ=YEARLY".PHP_EOL;
                     break;
             }
 
-            $rules[] = "INTERVAL={$event->repetition->interval}";
+            $rules[] = "INTERVAL={$event->repetition->interval}{PHP_EOL}";
 
             if ($event->repetition->count) {
-                $rules[] = "COUNT={$event->repetition->count}";
+                $rules[] = "COUNT={$event->repetition->count}{PHP_EOL}";
             }
 
             $rRule = "\nRRULE:" . implode(";", $rules);
@@ -80,22 +78,22 @@ CATEGORY:{$postEvent->post->type->plural}";
             $startDate = date('Ymd', $event->startDate);
             $endDate   = date('Ymd', $event->endDate + 86400);
 
-            return "BEGIN:VEVENT
-UID:{$event->id}
-DTSTART;VALUE=DATE:{$startDate}
-DTEND;VALUE=DATE:{$endDate}
-SUMMARY:{$event->title}{$rRule}{$postData}
-END:VEVENT";
+            return "BEGIN:VEVENT{PHP_EOL}
+UID:{$event->id}{PHP_EOL}
+DTSTART;VALUE=DATE:{$startDate}{PHP_EOL}
+DTEND;VALUE=DATE:{$endDate}{PHP_EOL}
+SUMMARY:{$event->title}{$rRule}{$postData}{PHP_EOL}
+END:VEVENT{PHP_EOL}";
 
         } else {
 
-            return "BEGIN:VEVENT
-UID:{$event->id}
-DTSTAMP:{$creationDate}
-DTSTART:{$startDate}
-DTEND:{$endDate}
-SUMMARY:{$event->title}{$rRule}{$postData}
-END:VEVENT";
+            return "BEGIN:VEVENT{PHP_EOL}
+UID:{$event->id}{PHP_EOL}
+DTSTAMP:{$creationDate}{PHP_EOL}
+DTSTART:{$startDate}{PHP_EOL}
+DTEND:{$endDate}{PHP_EOL}
+SUMMARY:{$event->title}{$rRule}{$postData}{PHP_EOL}
+END:VEVENT{PHP_EOL}";
         }
 
     }
@@ -108,15 +106,15 @@ END:VEVENT";
         }
         $allEvents = implode("\n", $allEvents);
 
-        return "BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Phidias//NONSGML Phidias Academico//EN
-NAME:Phidias Academico
-X-WR-CALNAME:Phidias Academico
-DESCRIPTION:Mis eventos en el colegio
-X-WR-CALDESC:Mis eventos en el colegio
+        return "BEGIN:VCALENDAR{PHP_EOL}
+VERSION:2.0{PHP_EOL}
+PRODID:-//Phidias//NONSGML Phidias Academico//EN{PHP_EOL}
+NAME:Phidias Academico{PHP_EOL}
+X-WR-CALNAME:Phidias Academico{PHP_EOL}
+DESCRIPTION:Mis eventos en el colegio{PHP_EOL}
+X-WR-CALDESC:Mis eventos en el colegio{PHP_EOL}
 {$allEvents}
-END:VCALENDAR";
+END:VCALENDAR{PHP_EOL}";
     }
 
     public function main($eventId)
@@ -128,11 +126,11 @@ END:VCALENDAR";
 
         $icsEvent = self::toIcs($event);
 
-        return "BEGIN:VCALENDAR
-VERSION:2.0
-PRODID:-//Phidias//NONSGML Phidias Academico//EN
+        return "BEGIN:VCALENDAR{PHP_EOL}
+VERSION:2.0{PHP_EOL}
+PRODID:-//Phidias//NONSGML Phidias Academico//EN{PHP_EOL}
 {$icsEvent}
-END:VCALENDAR";
+END:VCALENDAR{PHP_EOL}";
     }
 
     public static function abc($htmlMsg)
@@ -149,7 +147,7 @@ END:VCALENDAR";
                 $new_lines[]=trim($line);
             }
         }
-        $desc = implode("\n\n ",$new_lines);
+        $desc = implode(PHP_EOL,$new_lines);
         
         return $desc;
     }
