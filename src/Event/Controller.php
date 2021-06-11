e4dd512b6856883136da7b2a9b6f0e8b76957e1d
jonatanherran <jonatan.herran@phidias.com.co>
2021-06-11 10:36:47 -0500

    Exportar calendario con Evaluaciones, tareas, exámenes y eventos para importarse en G.Calendar CASO 349

--- src/Event/Controller.php
+++ src/Event/Controller.php
@@ -4,15 +4,92 @@ namespace Phidias\Calendar\Event;
 use Phidias\Calendar\Event\Entity as Event;
 use Phidias\Calendar\Event\Repetition\Entity as Repetition;
 use Phidias\Calendar\Event\Repetition\Controller as RepetitionController;
+use GuzzleHttp\Client;
+use Phidias\Calendar\Post\Event\Entity as PostEvent;
+use Phidias\Post\Entity as Post;
+use Phidias\Post\Stub\Entity as Stub;
 
 class Controller
-{
+{   
     public function collection()
     {
      //something   
         return Event::collection()
             ->allAttributes()
             ->attribute("repetition", Repetition::single()->allAttributes());
+    }
+
+    public function getEvents($personId,$query)
+    {
+        
+        $selectedFeeds = explode(",",$query->feeds);
+        $startDate = '1000-01-01'; // 01-04-2021 00:00:00
+        $endDate = '3000-12-31'; // 31-04-2021 00:00:00
+
+        // $startDate = '2021-05-30'; // 01-04-2021 00:00:00
+        // $endDate = '2021-07-13'; // 31-04-2021 00:00:00
+
+        $host = "http://127.0.0.1:8080/V4";
+        // return "$host/v3/calendar/google/$personId/feed?start=$startDate&end=$endDate";
+        // exit;
+        $host = "$_SERVER[HTTP_HOST]/V4";
+
+        $output = Event::collection()
+            ->allAttributes()
+            ->attribute("repetition", Repetition::single()->allAttributes());
+
+        $output->attribute("postEvent", PostEvent::single()
+            ->notEmpty()
+            ->attribute("post", Post::single()
+                ->notEmpty()
+                ->attributes("id", "type", "thread", "author")
+                ->join("stub", Stub::single()->notEmpty(), "postEvent.post.stub.post = postEvent.post.id AND postEvent.post.stub.person = '$personId'")
+            )
+        );
+        $output->where("postEvent.post.author = :personId OR postEvent.post.stub.person = :personId", ["personId" => $personId]);
+        $output->where("postEvent.post.deleteDate IS NULL");
+        $output->where("postEvent.post.publishDate IS NOT NULL");
+
+        if (isset($query->type) && !empty($query->type)) {
+            $types = explode(",", trim($query->type));
+            $output->match("postEvent.post.type", $types);
+        }
+        if (isset($query->node)) {
+            $output->match("postEvent.post.node", $query->node);
+        }
+
+        $feeds = [];
+
+        if ( gettype( array_search("feed",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/people/$personId/calendar/feed?start=$startDate&end=$endDate")); }
+        if ( gettype( array_search("aula",$selectedFeeds) ) <> "boolean"  ){array_push($feeds,self::getApi("$host/1/communication/people/$personId/events?start=$startDate&end=$endDate")); }
+        if ( gettype( array_search("evGoogle",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/v3/calendar/google/$personId/feed?start=$startDate&end=$endDate")); }
+        if ( gettype( array_search("exams",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/v3/people/$personId/academic/exams/feed?start=$startDate&end=$endDate")); }
+        if ( gettype( array_search("assignments",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/v3/people/$personId/academic/assignments/feed?start=$startDate&end=$endDate")); }
+        if ( gettype( array_search("evaluations",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/v3/people/$personId/academic/evaluations/feed?start=$startDate&end=$endDate")); }
+        
+        // return self::getApi("$host/v3/calendar/google/$personId/feed?start=$startDate&end=$endDate");
+
+        $events = [];
+        foreach($feeds as $value){
+            for($i=0;$i<count($value);$i++){
+                $events[] = $value[$i];
+            }
+        }
+
+        return $events;
+    }
+
+    public static function getApi($url){
+        $client = new Client();
+        try{
+            $response = $client->get($url);
+        }
+        catch(\Exception $e){
+            return $e->getMessage();
+        }
+        $retVal = $response->getBody()->getContents();
+        // var_dump($response->getBody());
+        return json_decode($retVal);
     }
 
     public function feed($startDate, $endDate)
--- src/Event/Ics/Controller.php
+++ src/Event/Ics/Controller.php
@@ -1,13 +1,10 @@
 <?php
 namespace Phidias\Calendar\Event\Ics;
-
 use Phidias\Calendar\Event\Entity as Event;
 use Phidias\Calendar\Event\Repetition\Entity as Repetition;
-
 use Phidias\Post\Entity as Post;
 use Phidias\Core\Noun\Entity as PostType;
 use Phidias\Calendar\Post\Event\Entity as PostEvent;
-
 class Controller
 {
     public static function toIcs(Event $event)
@@ -15,7 +12,6 @@ class Controller
         $creationDate = gmdate('Ymd\THis\Z', $event->creationDate);
         $startDate    = gmdate('Ymd\THis\Z', $event->startDate);
         $endDate      = gmdate('Ymd\THis\Z', $event->endDate);
-
         $postEvent = PostEvent::single()
             ->attribute("post", Post::single()
                     ->allAttributes()
@@ -24,9 +20,7 @@ class Controller
             ->where("event = :evtId", ["evtId" => $event->id])
             ->find()
             ->first();
-
         $postData = "";
-        
         if($postEvent){
             $clean_description_html = html_entity_decode($postEvent->post->description);
             $clean_description = html_entity_decode($postEvent->post->description);
@@ -37,58 +31,46 @@ DESCRIPTION:{$clean_description}
 X-ALT-DESC;FMTTYPE=text/html:{$clean_description_html}
 CATEGORY:{$postEvent->post->type->plural}";
         }
-        
         if ($event->repetition) {
             $rules = [];
             switch ($event->repetition->frequency) {
                 case Repetition::FREQUENCY_DAILY:
                     $rules[] = "FREQ=DAILY";
                     break;
-
                 case Repetition::FREQUENCY_WEEKLY:
                     $rules[] = "FREQ=WEEKLY";
                     $rules[] = "BYDAY=" . strtoupper(substr(date("D", $event->startDate),0,2));
                     break;
-
                 case Repetition::FREQUENCY_MONTHLY_DAY:
                     $rules[] = "FREQ=MONTHLY";
                     $rules[] = "BYMONTHDAY=" . date("j", $event->startDate);
                     break;
-
                 case Repetition::FREQUENCY_MONTHLY_WEEKDAY:
                     $rules[] = "FREQ=MONTHLY";
                     $rules[] = "BYSETPOS=" . ceil(date("j", $event->startDate)/7);
                     break;
-
                 case Repetition::FREQUENCY_YEARLY:
                     $rules[] = "FREQ=YEARLY";
                     break;
             }
-
             $rules[] = "INTERVAL={$event->repetition->interval}";
-
             if ($event->repetition->count) {
                 $rules[] = "COUNT={$event->repetition->count}";
             }
-
             $rRule = "\nRRULE:" . implode(";", $rules);
         } else {
             $rRule = "";
         }
-
         if ($event->allDay) {
             $startDate = date('Ymd', $event->startDate);
             $endDate   = date('Ymd', $event->endDate + 86400);
-
             return "BEGIN:VEVENT
 UID:{$event->id}
 DTSTART;VALUE=DATE:{$startDate}
 DTEND;VALUE=DATE:{$endDate}
 SUMMARY:{$event->title}{$rRule}{$postData}
 END:VEVENT";
-
         } else {
-
             return "BEGIN:VEVENT
 UID:{$event->id}
 DTSTAMP:{$creationDate}
@@ -97,7 +79,6 @@ DTEND:{$endDate}
 SUMMARY:{$event->title}{$rRule}{$postData}
 END:VEVENT";
         }
-
     }
 
     public static function toCalendar(array $events)
@@ -107,7 +88,6 @@ END:VEVENT";
             $allEvents[] = self::toIcs($event);
         }
         $allEvents = implode("\n", $allEvents);
-
         return "BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Phidias//NONSGML Phidias Academico//EN
@@ -119,15 +99,95 @@ X-WR-CALDESC:Mis eventos en el colegio
 END:VCALENDAR";
     }
 
+    public static function toCalendarFromArray(array $events)
+    {
+        $allEvents = [];
+        foreach ($events as $event) {
+            $allEvents[] = self::toIcsFromArray($event);
+        }
+        $allEvents = implode("\n", $allEvents);
+        return "BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Phidias//NONSGML Phidias Academico//EN
+NAME:Phidias Academico
+X-WR-CALNAME:Phidias Academico
+DESCRIPTION:Mis eventos en el colegio
+X-WR-CALDESC:Mis eventos en el colegio
+{$allEvents}
+END:VCALENDAR";
+    } 
+
+    public static function toIcsFromArray (Object $event){
+        
+        if($event->post->type->plural=="evGoogle"){
+            $event->endDate = $event->endDate-86400;
+        }
+
+        $creationDate = gmdate('Ymd\THis\Z', $event->start);
+        $startDate    = gmdate('Ymd\THis\Z', $event->startDate);
+        $endDate      = gmdate('Ymd\THis\Z', $event->endDate);
+        $postData = "";
+            $clean_description = html_entity_decode(strip_tags($event->title));
+            $postData = "
+DESCRIPTION:{$clean_description}
+CATEGORY:{$event->post->type->plural}";
+        if ($event->repetition) {
+            $rules = [];
+            switch ($event->repetition->frequency) {
+                case Repetition::FREQUENCY_DAILY:
+                    $rules[] = "FREQ=DAILY";
+                    break;
+                case Repetition::FREQUENCY_WEEKLY:
+                    $rules[] = "FREQ=WEEKLY";
+                    $rules[] = "BYDAY=" . strtoupper(substr(date("D", $event->startDate),0,2));
+                    break;
+                case Repetition::FREQUENCY_MONTHLY_DAY:
+                    $rules[] = "FREQ=MONTHLY";
+                    $rules[] = "BYMONTHDAY=" . date("j", $event->startDate);
+                    break;
+                case Repetition::FREQUENCY_MONTHLY_WEEKDAY:
+                    $rules[] = "FREQ=MONTHLY";
+                    $rules[] = "BYSETPOS=" . ceil(date("j", $event->startDate)/7);
+                    break;
+                case Repetition::FREQUENCY_YEARLY:
+                    $rules[] = "FREQ=YEARLY";
+                    break;
+            }
+            $rules[] = "INTERVAL={$event->repetition->interval}";
+            if ($event->repetition->count) {
+                $rules[] = "COUNT={$event->repetition->count}";
+            }
+            $rRule = "\nRRULE:" . implode(";", $rules);
+        } else {
+            $rRule = "";
+        }
+        if ($event->allDay) {
+            $startDate = date('Ymd', $event->startDate);
+            $endDate   = date('Ymd', $event->endDate + 86400);
+            return "BEGIN:VEVENT
+UID:{$event->id}
+DTSTART;VALUE=DATE:{$startDate}
+DTEND;VALUE=DATE:{$endDate}
+SUMMARY:{$event->title}{$rRule}{$postData}
+END:VEVENT";
+        } else {
+            return "BEGIN:VEVENT
+UID:{$event->id}
+DTSTAMP:{$creationDate}
+DTSTART:{$startDate}
+DTEND:{$endDate}
+SUMMARY:{$event->title}{$rRule}{$postData}
+END:VEVENT";
+        }
+    }
+
     public function main($eventId)
     {
         $event = Event::single()
             ->allAttributes()
             ->attribute("repetition", Repetition::single()->allAttributes())
             ->fetch($eventId);
-
         $icsEvent = self::toIcs($event);
-
         return "BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Phidias//NONSGML Phidias Academico//EN
