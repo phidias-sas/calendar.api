d5c65e1ebf7f8843fe5a6de3ddbbe1922b75c283
jonatanherran <jonatan.herran@phidias.com.co>
2021-07-22 15:28:12 -0500

    https://phidias.atlassian.net/browse/CASO-349

--- src/Event/Controller.php
+++ src/Event/Controller.php
@@ -4,7 +4,6 @@ namespace Phidias\Calendar\Event;
 use Phidias\Calendar\Event\Entity as Event;
 use Phidias\Calendar\Event\Repetition\Entity as Repetition;
 use Phidias\Calendar\Event\Repetition\Controller as RepetitionController;
-use GuzzleHttp\Client;
 use Phidias\Calendar\Post\Event\Entity as PostEvent;
 use Phidias\Post\Entity as Post;
 use Phidias\Post\Stub\Entity as Stub;
@@ -21,48 +20,53 @@ class Controller
 
     public function getEvents($personId,$query)
     {
-        
         $selectedFeeds = explode(",",$query->feeds);
         $startDate = '1000-01-01'; // 01-04-2021 00:00:00
         $endDate = '3000-12-31'; // 31-04-2021 00:00:00
 
-        // $host = "http://127.0.0.1:8080/V4";
-        $host = "$_SERVER[HTTP_HOST]";
-
-        $output = Event::collection()
-            ->allAttributes()
-            ->attribute("repetition", Repetition::single()->allAttributes());
-
-        $output->attribute("postEvent", PostEvent::single()
-            ->notEmpty()
-            ->attribute("post", Post::single()
-                ->notEmpty()
-                ->attributes("id", "type", "thread", "author")
-                ->join("stub", Stub::single()->notEmpty(), "postEvent.post.stub.post = postEvent.post.id AND postEvent.post.stub.person = '$personId'")
-            )
-        );
-        $output->where("postEvent.post.author = :personId OR postEvent.post.stub.person = :personId", ["personId" => $personId]);
-        $output->where("postEvent.post.deleteDate IS NULL");
-        $output->where("postEvent.post.publishDate IS NOT NULL");
-
-        if (isset($query->type) && !empty($query->type)) {
-            $types = explode(",", trim($query->type));
-            $output->match("postEvent.post.type", $types);
-        }
-        if (isset($query->node)) {
-            $output->match("postEvent.post.node", $query->node);
-        }
-
         $feeds = [];
+        // CONSUMO POR API
+        if ( gettype( array_search("feed",$selectedFeeds) ) <> "boolean" ){
+            // array_push($feeds,self::getApi("$host/people/$personId/calendar/feed?start=$startDate&end=$endDate",$query->token)); 
+            $output = $this->collection();
+            // var_dump($output);
+            $output->attribute("postEvent", PostEvent::single()
+                    ->notEmpty()
+                    ->attribute("post", Post::single()
+                        ->notEmpty()
+                        ->attributes("id", "type", "thread", "author")
+                        ->join("stub", Stub::single()->notEmpty(), "postEvent.post.stub.post = postEvent.post.id AND postEvent.post.stub.person = '$personId'")
+                    )
+            );
+            $output->where("postEvent.post.author = :personId OR postEvent.post.stub.person = :personId", ["personId" => $personId]);
+            $output->where("postEvent.post.deleteDate IS NULL");
+            $output->where("postEvent.post.publishDate IS NOT NULL");
 
-        if ( gettype( array_search("feed",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/people/$personId/calendar/feed?start=$startDate&end=$endDate")); }
-        if ( gettype( array_search("aula",$selectedFeeds) ) <> "boolean"  ){array_push($feeds,self::getApi("$host/1/communication/people/$personId/events?start=$startDate&end=$endDate")); }
-        if ( gettype( array_search("evGoogle",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/v3/calendar/google/$personId/feed?start=$startDate&end=$endDate")); }
-        if ( gettype( array_search("exams",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/v3/people/$personId/academic/exams/feed?start=$startDate&end=$endDate")); }
-        if ( gettype( array_search("assignments",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/v3/people/$personId/academic/assignments/feed?start=$startDate&end=$endDate")); }
-        if ( gettype( array_search("evaluations",$selectedFeeds) ) <> "boolean" ){array_push($feeds,self::getApi("$host/v3/people/$personId/academic/evaluations/feed?start=$startDate&end=$endDate")); }
-        
-        // return self::getApi("$host/v3/calendar/google/$personId/feed?start=$startDate&end=$endDate");
+            if (isset($query->type) && !empty($query->type)) {
+                $types = explode(",", trim($query->type));
+                $output->match("postEvent.post.type", $types);
+            }
+
+            if (isset($query->node)) {
+                $output->match("postEvent.post.node", $query->node);
+            }
+
+            $allEvents = $output->find()->fetchAll();
+            array_push($feeds,$allEvents);
+        }
+        if ( gettype( array_search("aula",$selectedFeeds) ) <> "boolean"  ){
+            // array_push($feeds,self::getApi("$host/1/communication/people/$personId/events?start=$startDate&end=$endDate",$query->token)); 
+            $query->start=$startDate;
+            $query->end  =$endDate;
+            array_push($feeds, \Phidias\Core\Communication\Node\Post\Controller::getEvents($personId, $query) );
+        }
+        // LLAMADO AL CONTROLLER
+        if ( gettype( array_search("evGoogle",$selectedFeeds) ) <> "boolean" ){array_push($feeds, \Phidias\V3\Calendar\Google\Controller::googleEvents($personId,$startDate, $endDate));}
+        if ( gettype( array_search("exams",$selectedFeeds) ) <> "boolean" ){array_push($feeds, \Phidias\V3\Academic\Exam\Controller::feed($personId, $startDate, $endDate) );}
+        if ( gettype( array_search("assignments",$selectedFeeds) ) <> "boolean" ){array_push($feeds, \Phidias\V3\Academic\Assignment\Controller::feed($personId, $startDate, $endDate) );}
+        if ( gettype( array_search("evaluations",$selectedFeeds) ) <> "boolean" ){array_push($feeds, \Phidias\V3\Academic\Evaluation\Controller::feed($personId, $startDate, $endDate) );}
+
+        // return $feeds;
 
         $events = [];
         foreach($feeds as $value){
@@ -72,19 +76,6 @@ class Controller
         }
 
         return $events;
-    }
-
-    public static function getApi($url){
-        $client = new Client();
-        try{
-            $response = $client->get($url);
-        }
-        catch(\Exception $e){
-            return $e->getMessage();
-        }
-        $retVal = $response->getBody()->getContents();
-        // var_dump($response->getBody());
-        return json_decode($retVal);
     }
 
     public function feed($startDate, $endDate)
--- src/Event/Ics/Controller.php
+++ src/Event/Ics/Controller.php
@@ -181,69 +181,6 @@ END:VEVENT";
         }
     }
 
-    public static function toIcsFromArray (Object $event){
-        
-        if($event->post->type->plural=="evGoogle"){
-            $event->endDate = $event->endDate-86400;
-        }
-
-        $creationDate = gmdate('Ymd\THis\Z', $event->start);
-        $startDate    = gmdate('Ymd\THis\Z', $event->startDate);
-        $endDate      = gmdate('Ymd\THis\Z', $event->endDate);
-        $postData = "";
-            $clean_description = html_entity_decode(strip_tags($event->title));
-            $postData = "
-DESCRIPTION:{$clean_description}
-CATEGORY:{$event->post->type->plural}";
-        if ($event->repetition) {
-            $rules = [];
-            switch ($event->repetition->frequency) {
-                case Repetition::FREQUENCY_DAILY:
-                    $rules[] = "FREQ=DAILY";
-                    break;
-                case Repetition::FREQUENCY_WEEKLY:
-                    $rules[] = "FREQ=WEEKLY";
-                    $rules[] = "BYDAY=" . strtoupper(substr(date("D", $event->startDate),0,2));
-                    break;
-                case Repetition::FREQUENCY_MONTHLY_DAY:
-                    $rules[] = "FREQ=MONTHLY";
-                    $rules[] = "BYMONTHDAY=" . date("j", $event->startDate);
-                    break;
-                case Repetition::FREQUENCY_MONTHLY_WEEKDAY:
-                    $rules[] = "FREQ=MONTHLY";
-                    $rules[] = "BYSETPOS=" . ceil(date("j", $event->startDate)/7);
-                    break;
-                case Repetition::FREQUENCY_YEARLY:
-                    $rules[] = "FREQ=YEARLY";
-                    break;
-            }
-            $rules[] = "INTERVAL={$event->repetition->interval}";
-            if ($event->repetition->count) {
-                $rules[] = "COUNT={$event->repetition->count}";
-            }
-            $rRule = "\nRRULE:" . implode(";", $rules);
-        } else {
-            $rRule = "";
-        }
-        if ($event->allDay) {
-            $startDate = date('Ymd', $event->startDate);
-            $endDate   = date('Ymd', $event->endDate + 86400);
-            return "BEGIN:VEVENT
-UID:{$event->id}
-DTSTART;VALUE=DATE:{$startDate}
-DTEND;VALUE=DATE:{$endDate}
-SUMMARY:{$event->title}{$rRule}{$postData}
-END:VEVENT";
-        } else {
-            return "BEGIN:VEVENT
-UID:{$event->id}
-DTSTAMP:{$creationDate}
-DTSTART:{$startDate}
-DTEND:{$endDate}
-SUMMARY:{$event->title}{$rRule}{$postData}
-END:VEVENT";
-        }
-    }
 
     public function main($eventId)
     {
